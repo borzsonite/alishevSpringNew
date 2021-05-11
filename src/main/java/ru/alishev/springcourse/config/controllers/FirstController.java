4354a88f0624278a583712844e8ef75dcf954cf9
Sergey <borzsonite@gmail.com>
2021-05-11 20:57:17 +0300

    lesson19 end

--- src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/alishev/springcourse/config/controllers/FirstController.java
@@ -3,18 +3,23 @@ package ru.alishev.springcourse.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/firstt") // указывает на необходимость данного префикса в строке урл
+//@RequestMapping("/first") // указывает на необходимость данного префикса в строке урл
 public class FirstController {
 
    @GetMapping("/hello") // после набора в строке урл /hello
-    public String helloPage() {
-        return "/first/hello"; // вызовется страница  WEB-INF/views/first/hello.html
+    public String helloPage(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname", required = false) String surname) {
+
+       System.out.println("Hello " + name + " " + surname);
+        return "first/hello"; // вызовется страница  WEB-INF/views/first/hello.html
     }
 
     @GetMapping("/goodbuy")
     public String goodbuyPage() {
-       return "/first/goodbuy";
+       return "first/goodbuy";
     }
 }
